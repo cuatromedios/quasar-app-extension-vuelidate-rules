622364c78cabe5b6f55a1a7db6d444b87e178d76
Ramses <ramses@cuatromedios.com>
2021-07-11 17:32:31 -0500

    Version compatible with Quasar 2 / Vue 3 Added requiredIf and requiredUnless rules

--- src/boot/register-vuelidate-rules.js
+++ src/boot/register-vuelidate-rules.js
@@ -1,7 +1,7 @@
-import * as methods from 'vuelidate/lib/validators'
+import * as methods from '@vuelidate/validators'
 
-export default ({ Vue }) => {
-  Vue.prototype.$rules = {
+export default ({ app }) => {
+  app.config.globalProperties.$rules = {
     is (value, message) {
       message = message !== undefined ? message : false
       return (val) => {
@@ -20,71 +20,72 @@ export default ({ Vue }) => {
       }
     },
     required (message = false) {
-      return (val) => methods.required(val) || message
+      return (val) => methods.required.$validator(val) || message
     },
-    minLength (length, message) {
-      message = message !== undefined ? message : false
-      return (val) => methods.minLength(length)(val) || message
+    requiredIf (ref, message = false) {
+      return (val) => methods.requiredIf(ref).$validator(val) || message
     },
-    maxLength (length, message) {
-      message = message !== undefined ? message : false
-      return (val) => methods.maxLength(length)(val) || message
+    requiredUnless (ref, message = false) {
+      return (val) => methods.requiredUnless(ref).$validator(val) || message
     },
-    minValue (value, message) {
-      message = message !== undefined ? message : false
-      return (val) => methods.minValue(value)(val) || message
+    minLength (length, message = false) {
+      return (val) => methods.minLength(length).$validator(val) || message
     },
-    maxValue (value, message) {
-      message = message !== undefined ? message : false
-      return (val) => methods.maxValue(value)(val) || message
+    maxLength (length, message = false) {
+      return (val) => methods.maxLength(length).$validator(val) || message
     },
-    between (min, max, message) {
-      message = message !== undefined ? message : false
-      return (val) => methods.between(min, max)(val) || message
+    minValue (value, message = false) {
+      return (val) => methods.minValue(value).$validator(val) || message
+    },
+    maxValue (value, message = false) {
+      return (val) => methods.maxValue(value).$validator(val) || message
+    },
+    between (min, max, message = false) {
+      return (val) => methods.between(min, max).$validator(val) || message
     },
     alpha (message = false) {
-      return (val) => methods.alpha(val) || message
+      return (val) => methods.alpha.$validator(val) || message
     },
     alphaNum (message = false) {
-      return (val) => methods.alphaNum(val) || message
+      return (val) => methods.alphaNum.$validator(val) || message
     },
     numeric (message = false) {
-      return (val) => methods.numeric(val) || message
+      return (val) => methods.numeric.$validator(val) || message
     },
     integer (message = false) {
-      return (val) => methods.integer(val) || message
+      return (val) => methods.integer.$validator(val) || message
     },
     decimal (message = false) {
-      return (val) => methods.decimal(val) || message
+      return (val) => methods.decimal.$validator(val) || message
     },
     email (message = false) {
-      return (val) => methods.email(val) || message
+      return (val) => methods.email.$validator(val) || message
     },
     ipAddress (message = false) {
-      return (val) => methods.ipAddress(val) || message
+      return (val) => methods.ipAddress.$validator(val) || message
     },
     macAddress (separator = ':', message = false) {
-      return (val) => methods.macAddress(separator)(val) || message
+      return (val) => methods.macAddress.$validator(separator)(val) || message
     },
     url (message = false) {
-      return (val) => methods.url(val) || message
+      return (val) => methods.url.$validator(val) || message
     },
     or (...args) {
       let message = false
       if (typeof args[args.length - 1] === 'string') {
         message = args.pop()
       }
-      return (val) => methods.or(...args)(val) || message
+      return (val) => methods.or(...args).$validator(val) || message
     },
     and (...args) {
       let message = false
       if (typeof args[args.length - 1] === 'string') {
         message = args.pop()
       }
-      return (val) => methods.and(...args)(val) || message
+      return (val) => methods.and(...args).$validator(val) || message
     },
     not (rule, message = false) {
-      return (val) => methods.not(rule)(val) || message
+      return (val) => methods.not(rule).$validator(val) || message
     },
     sameAs(locator, message = false) {
       return (val) => val == locator || message
